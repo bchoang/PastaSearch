ddd37e2210d1bbceeb2e9fea87296f22264c7929
Jackson Darrow <jackson.darrow@gmail.com>
2015-12-10 21:16:38 -0800

    second commit

---
+++ crawler.js
@@ -0,0 +1,50 @@
+var pg = require('pg');
+var conString = "postgres://jackson:________@localhost/copypasta";
+var request = require('request');
+
+// this initializes a connection pool
+// it will keep idle connections open for a (configurable) 30 seconds
+// and set a limit of 20 (also configurable)
+
+function addPost(id, selftext, score){
+    pg.connect(conString, function(err, client, done) {
+        if(err) return console.error('error fetching client from pool', err);
+
+        client.query('SELECT id from posts WHERE id=$1', [id], function(err, checkResult) {
+            done(); //call `done()` to release the client back to the pool
+            if(err) return console.error('error running query', err);
+            if (checkResult.rows.length !== 0) return console.log('that post has been entered already');
+
+            client.query('INSERT INTO posts (id, selftext, score) VALUES ($1, $2, $3::int);', [id, selftext, score], function(err, result) {
+                done(); //call `done()` to release the client back to the pool
+                if(err) return console.error('error running query', err);
+            });
+
+        });
+    });
+}
+
+function fetchData (url, next, timesLeft){
+    if (timesLeft < 0) return;
+    var currentPage = url + "&count=25&after=" + next;
+    console.log(currentPage);
+    request(currentPage, function (err, response, body){
+        if (!err && response.statusCode == 200) {
+            var data = JSON.parse(body).data;
+            fetchData (url, data.after, timesLeft-1);
+
+            var posts = data.children;
+            for (var i = 0, l = posts.length; i < l; i ++){
+                var post = posts[i];
+                var selftext = post.data.selftext;
+                var score = post.data.score;
+                var id = post.data.id;
+
+                addPost(id, selftext, score);
+            }
+        }
+    });
+}
+
+var baseURL = "https://www.reddit.com/r/copypasta/top.json?sort=top&t=all";
+fetchData(baseURL, "", 15);
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.sendFile('index.html');
+});
+
+var pg = require('pg');
+var conString = "postgres://jackson:________@localhost/copypasta";
+
+router.get('/get', function (req, res, next){
+    var searchText = req.query.searchText;
+    var page = req.query.page;
+
+    var pattern = "%(" + searchText.split(' ').join('|') + ")%";
+
+    var queryText = "SELECT selftext FROM posts WHERE selftext SIMILAR TO $1 ORDER BY score DESC LIMIT 10";
+    if (page) queryText += "OFFSET " + page*10;
+
+    pg.connect(conString, function(err, client, done) {
+        if(err) return console.error('error fetching client from pool', err);
+    
+        client.query(queryText, [pattern], function(err, result) {
+            done(); //call `done()` to release the client back to the pool
+            if(err) return console.error('error running query', err);
+
+            if (result.rows[0])
+            res.send(result.rows);
+        });
+    });
+});
+
+module.exports = router;
